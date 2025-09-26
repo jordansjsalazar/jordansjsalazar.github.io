92b04228d0f3e0fd4fce99b972e3ad3e1cd07127
Jorge <jordansjsalazar@gmail.com>
2025-09-26 17:56:07 +0200

    trying to do light mode

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-// portfolio
-$('.gallery img').click(function() {
-    var itemID = $(this).attr('href');
-    $('.gallery ul').addClass('item_open');
-    $(itemID).addClass('item_open');
-    return false;
-});
-
-$('.close').click(function() {
-    $('.port, .gallery ul').removeClass('item_open');
-    return false;
-});
-
-$(".gallery img").click(function() {
-    $('html, body').animate({
-        scrollTop: parseInt($("#top").offset().top)
-    }, 400);
-});
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("light-mode");
+} 
